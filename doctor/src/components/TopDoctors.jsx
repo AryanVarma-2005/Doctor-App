a2e27a3d99dae87f098d87c46f5d4bb9c04a0504
AryanVarma-2005 <dantuluri.aryanvarma@gmail.com>
2026-03-16 01:07:22 +0530

    ui

--- doctor/src/components/TopDoctors.jsx
+++ doctor/src/components/TopDoctors.jsx
@@ -25,7 +25,7 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button onClick={() => { navigate('/doctors'); scrollTo(0, 0) }} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">more</button>
+      <button onClick={() => { navigate('/doctors'); scrollTo(0, 0) }} className="bg-blue-300 text-black-900 px-12 py-3 rounded-full mt-10 cursor-pointer">more</button>
     </div>
   )
 }
