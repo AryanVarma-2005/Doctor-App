0080191f0c959848621ca39d57d413bdd0735ff7
AryanVarma-2005 <dantuluri.aryanvarma@gmail.com>
2026-03-17 14:54:07 +0530

    last

---
+++ doctor/src/components/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Banner = () => {
+  return (
+<div className="flex bg-[#6B6CFF] rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10">        {/*---Left Side---*/}
+        <div>
+            <div>
+                <p>Book Appointment</p>
+                <p>With 100+ Trusted Doctors</p>
+            </div>
+            <button>Create account</button>
+        </div>
+                {/*---Right Side---*/}
+        <div>
+            <img src={assets.appointment_img} alt="" />
+        </div>
+      
+    </div>
+  )
+}
+
+export default Banner
